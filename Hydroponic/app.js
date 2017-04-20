5b1c89485d97407692d6e6097f8032f5ead714cd
thach <hbathach@gmail.com>
2017-04-21 00:08:10 +0700

    change database to postgres

--- Hydroponic/app.js
+++ Hydroponic/app.js
@@ -31,6 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/angular',express.static(path.join(__dirname, 'angular')));
 
 
 app.use(cors());
