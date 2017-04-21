0fec979e3233b2892a945ea3918b013d84141d3e
thach <hbathach@gmail.com>
2017-04-21 11:02:47 +0700

    add some font file, test database

--- Hydroponic/app.js
+++ Hydroponic/app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/angular',express.static(path.join(__dirname, 'angular')));
+app.use('/angular',express.static(path.join(__dirname, 'angularjs')));
 
 
 app.use(cors());
