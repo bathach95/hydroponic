d5fe61dbde02cab1615a823083d13de82b7a1e8a
chitayta <chitayta@gmail.com>
2017-05-23 00:44:47 +0700

    add crop image gallery UI, edit crop overview modal

--- Hydroponic/app.js
+++ Hydroponic/app.js
@@ -63,6 +63,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-models.sequelize.sync({force: true});
+//models.sequelize.sync({force: true});
+models.sequelize.sync();
 
 module.exports = app;
